435e257a6839b63c0f8ed59a580dfee266552a05
Th3At0nic <islammdrahatul@gmail.com>
2021-06-07 16:14:07 +0600

    frist commit

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import Constants from "expo-constants";
+import { Alert, StyleSheet, View } from "react-native";
+import Topbar from "./Components/Topbar";
+import axios from "axios";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  
+  async function fetchUsers() {
+    try {
+      const { data } = await axios.get(
+        "https://randomsuer.me/api/?gender=female&results=50"
+      );
+      setUsers(data.results);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error getting users", " ", [
+        { text: "Retry", onPress: () => fetchUsers() },
+      ]);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Topbar />
+      <View style={styles.swipes}></View>
     </View>
   );
 }
@@ -14,8 +36,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+  },
+  swipes: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
   },
 });
